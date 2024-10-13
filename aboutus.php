402066d6cb5ed69cee3d7f33338ee8cb614e5342
goncasfausto04 <20231721@novaims.unl.pt>
2024-10-14 00:08:56 +0100

    <zx

--- aboutus.php
+++ aboutus.php
@@ -1,8 +1,3 @@
-<?php
-include 'session.php';
-?>
-
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -12,6 +7,46 @@ include 'session.php';
     <link rel="stylesheet" href="template.css"> <!-- Link to your CSS file -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <!-- Import the Allison font from Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Allison&display=swap">
+    <style>
+        .memeorgy-title {
+            font-family: 'Allison', cursive;
+            color: white;
+            font-size: 200px;
+            margin-bottom: 40px;
+            text-align: center;
+            transition: font-size 0.6s ease, margin-top 0.6s ease;
+            background-color: gray;
+            width: fit-content;
+            margin: auto;
+            padding: 50px;
+            border-radius: 50%;
+        }
+
+        .memeorgy-title .meme {
+            color: black;
+        }
+
+        .memeorgy-title .orgy {
+            color: #A02727;
+        }
+
+        /* Media query for smaller screens */
+        @media (max-width: 768px) {
+            .memeorgy-title {
+                font-size: 100px;
+                padding: 30px;
+            }
+        }
+
+        @media (max-width: 480px) {
+            .memeorgy-title {
+                font-size: 80px;
+                padding: 20px;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -29,7 +64,8 @@ include 'session.php';
     </header>
 
     <h1>About Us</h1>
-
+    <h1 class="memeorgy-title"><span class="meme">Fucking</span><span class="orgy">Niggers</span></h1>
+    
     <footer>
         <div class="footer-content">
             <h>Meme Orgy</h>
