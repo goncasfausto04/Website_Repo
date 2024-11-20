a5aff9865771a6789ae7e11c1e8a88c4529a48ea
Gonçalo Gouveia <109439282+GoncaloGouveia04@users.noreply.github.com>
2024-11-20 17:16:49 +0000

    Implement minecraft page (#3)
    * added files
    
    * aaa
    
    * saving again
    
    ---------
    
    Co-authored-by: Gonçalo Gouveia <goncalo.gouveia@glooma.pt>

--- login.php
+++ login.php
@@ -3,25 +3,31 @@ session_start();
 
 include 'db_connection.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
     $password_input = $_POST['password'];
 
-    $sql = "SELECT * FROM accounts WHERE password = ?";
+    $sql = "SELECT * FROM accounts WHERE username = ?";
     $stmt = $conn->prepare($sql);
 
     if (!$stmt) {
         die("Statement preparation failed: " . $conn->error);
     }
 
-    $stmt->bind_param("s", $password_input);
+    $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username'] = $user['user'];
-        header("Location: homepage.php");
-        exit();
+        if (password_verify($password_input, $user['password'])) {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $user['username'];
+            header("Location: homepage.php");
+            exit();
+        } else {
+            header("Location: index.html?error=invalid");
+            exit();
+        }
     } else {
         header("Location: index.html?error=invalid");
         exit();
